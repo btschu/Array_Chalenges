f4a533149b0da33940e968d0b984bdb7bd5b0714
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-11-19 12:40:39 -0700

    added how the instructor solved each challenge

--- #1_alwaysHungry.js
+++ #1_alwaysHungry.js
@@ -3,7 +3,6 @@
 
 function alwaysHungry(arr) {
     var count = 0;
-    // your code here
     for (var i = 0; i < arr.length; i++) {
         if(arr[i] == "food") {
             count++;
@@ -19,3 +18,5 @@ alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
+
+// my answer was the same as the solution video
